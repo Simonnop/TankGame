44e28046f5378292142d6b42e4a362579774f781
SuRuixian <741617293@qq.com>
2022-10-13 12:39:46 +0800

    动态设置界面

--- src/main/java/group/su/view/WelMenuPanel.java
+++ src/main/java/group/su/view/WelMenuPanel.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class WelMenuPanel extends JPanel {
     @Override
-    public void print(Graphics g) {
+    public void paint(Graphics g) {
         super.paint(g);
         g.setColor(Color.YELLOW);
         g.fillRect(0,0,800,700);
